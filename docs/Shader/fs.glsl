be4f79a359f53ef9381d9d8785d9ac931ce28c0f
hjlld <vickihjl@qq.com>
2019-09-16 12:34:22 +0800

    add

---
+++ docs/Shader/fs.glsl
@@ -0,0 +1,20 @@
+precision highp float;
+
+// Attributes
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+
+// Uniforms
+uniform mat4 worldViewProjection;
+
+// Varying
+varying vec4 vPosition;
+varying vec3 vNormal;
+varying vec2 vUV;
+
+void main(void) {
+    gl_Position = worldViewProjection * vec4(position, 1.0);
+
+    vUV = uv;
+}
--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,6 @@ module.exports = {
     },
     publicPath: process.env.NODE_ENV === 'production'
     ? '/modern-web-2019/'
-    : '/'
+    : '/',
+    outputDir: './docs'
   }
